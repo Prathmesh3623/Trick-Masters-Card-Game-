4639b2376224d515363496c3ed16e249a6996553
Prathmesh Shinde <sprathmesh569@gmail.com>
2026-03-23 21:42:24 +0530

    Fix server port logging statement

--- Server/server.js
+++ Server/server.js
@@ -252,6 +252,8 @@ for (let roomId in rooms) {
 
 })
 
+const PORT = process.env.PORT || 3001;
+
 server.listen(PORT, () => {
-console.log("Server running on port", PORT)
-})
+  console.log("Server running on port", PORT);
+});
